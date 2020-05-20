d84d644f15bc0978b04011b52618371f87b94283
oldfartdeveloper <scottnelsonsmith@gmail.com>
2020-05-20 10:44:51 -0700

    Initial test setup

--- exercises/chapter8/test/Main.purs
+++ exercises/chapter8/test/Main.purs
@@ -1,11 +1,14 @@
 module Test.Main where
 
 import Prelude
-
 import Effect (Effect)
-import Effect.Class.Console (log)
+import Test.Unit (suite, test)
+import Test.Unit.Assert as Assert
+import Test.Unit.Main (runTest)
 
 main :: Effect Unit
-main = do
-  log "🍝"
-  log "You should add some tests."
+main =
+  runTest do
+    suite "Verifying unit test environment" do
+      test "Initial passing test"
+        $ Assert.equal true true
