273bb188f4ea4886ace00dbd3c7d542a2626504b
oldfartdeveloper <scottnelsonsmith@gmail.com>
2020-05-20 15:22:14 -0700

    Section 'Folding With Monads' has unit tests

--- exercises/chapter8/test/Main.purs
+++ exercises/chapter8/test/Main.purs
@@ -1,7 +1,14 @@
 module Test.Main where
 
 import Prelude
+import Data.List
+  ( List(..)
+  , foldM
+  , (:)
+  )
+import Data.Maybe (Maybe(..))
 import Effect (Effect)
+import Test.Solutions (safeDivide)
 import Test.Unit (suite, test)
 import Test.Unit.Assert as Assert
 import Test.Unit.Main (runTest)
@@ -12,3 +19,10 @@ main =
     suite "Verifying unit test environment" do
       test "Initial passing test"
         $ Assert.equal true true
+    suite "Testing foldM and safeDivide" do
+      test "[5, 2, 2] has a Just answer"
+        $ Assert.equal (Just 5)
+        $ foldM safeDivide 100 (5 : 2 : 2 : Nil)
+      test "[5, 0, 2] has a Nothing answer"
+        $ Assert.equal (Nothing)
+        $ foldM safeDivide 100 (5 : 0 : 2 : Nil)
