b4b4790b8c239c37106db224ae9e349c1b0f7c08
oldfartdeveloper <scottnelsonsmith@gmail.com>
2020-05-20 20:23:12 -0700

    First Ch 8 tests

--- exercises/chapter8/test/Main.purs
+++ exercises/chapter8/test/Main.purs
@@ -1,11 +1,28 @@
 module Test.Main where
 
 import Prelude
-
+import Data.List
+  ( List(..)
+  , foldM
+  , (:)
+  )
+import Data.Maybe (Maybe(..))
 import Effect (Effect)
-import Effect.Class.Console (log)
+import Test.Solutions (safeDivide)
+import Test.Unit (suite, test)
+import Test.Unit.Assert as Assert
+import Test.Unit.Main (runTest)
 
 main :: Effect Unit
-main = do
-  log "🍝"
-  log "You should add some tests."
+main =
+  runTest do
+    suite "Verifying unit test environment" do
+      test "Initial passing test"
+        $ Assert.equal true true
+    suite "Testing foldM and safeDivide" do
+      test "[5, 2, 2] has a Just answer"
+        $ Assert.equal (Just 5)
+        $ foldM safeDivide 100 (5 : 2 : 2 : Nil)
+      test "[5, 0, 2] has a Nothing answer"
+        $ Assert.equal (Nothing)
+        $ foldM safeDivide 100 (5 : 0 : 2 : Nil)
---
+++ exercises/chapter8/test/Solutions.purs
@@ -0,0 +1,23 @@
+module Test.Solutions where
+
+import Prelude
+import Data.List (List(..), concatMap, (:))
+import Data.Maybe (Maybe(..))
+
+class
+  Apply m <= Bind m where
+  bind :: forall a b. m a -> (a -> m b) -> m b
+
+class (Applicative m, Bind m) <= Monad m
+
+instance bindList :: Bind List where
+  bind xs f = concatMap f xs
+
+instance bindMaybe :: Bind Maybe where
+  bind Nothing _ = Nothing
+  bind (Just a) f = f a
+
+safeDivide :: Int -> Int -> Maybe Int
+safeDivide _ 0 = Nothing
+
+safeDivide a b = Just (a / b)
